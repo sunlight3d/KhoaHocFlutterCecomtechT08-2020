5ba2c87f7993f7d1c1030fe4269636c268e6fe99
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-08-14 07:17:23 +0700

    update

--- nodejsBackend/routings/user.js
+++ nodejsBackend/routings/user.js
@@ -1,47 +1,103 @@
-const jsonResponse = require('./jsonResponse')
+const {
+	jsonResponse,
+	STATUS_SUCCESS,
+	STATUS_FAILED
+} = require('./jsonResponse')
 const { Op } = require("sequelize")
 const validator = require('../helpers/validator')
 const {
 	hashPassword,
 	checkPassword,
-	generateRandomString
+	generateRandomString,
+	checkPassword
 } = require('../helpers/utilities')
 
 module.exports = ({router, i18n, sequelize}) => {
 	const User = require('../models/user')(sequelize)
 	const validationRule = {
         "email": "required|email",        
-        "password": "required|string|min:2",
-        "gender": "string"
+        "password": "required|string|min:2",        
     }    	
 	router.post('/register', async (request, response) => {
 		validator(request.body, validationRule, {}, (err, status) => {
 			if (!status) {
 				jsonResponse({
 					response,
-					status: 'failed',					
+					status: STATUS_FAILED,					
 					message: `Validation error: ${err}`
 				})
 			} else {
-				next()
+				const {email, password} = request.body
+				try {
+					const newUser = await User.create({
+						email, 
+						password: hashPassword(password),
+						tokenKey: generateRandomString(40),				
+					})
+					const {email, id, name, tokenKey} = newUser
+					jsonResponse({
+						response,
+						status: STATUS_SUCCESS,
+						message: 'Register user successfully',
+						data: {email, id, name, tokenKey}
+					})
+				}catch(exception) {
+					jsonResponse({
+						response,
+						status: STATUS_FAILED,	
+						message: `Cannot register new user: ${err}`,
+						data: {}
+					})
+				}
 			}
-		})
-		try {
-			const newUser = await User.create({
-				name, email, 
-				password: hashPassword(password),
-				tokenKey: generateRandomString(40),				
-			})
-			const {email, id, name, tokenKey} = newUser
-			jsonResponse({
-				response,
-				status: 'ok',
-				message: 'Register user successfully',
-				data: {email, id, name, tokenKey}
-			})
-		}catch(exception) {
+		})				
 
-		}
-
+	})		
+	router.post('/login', async (request, response) => {
+		validator(request.body, validationRule, {}, (err, status) => {
+			if (!status) {
+				jsonResponse({
+					response,
+					status: STATUS_FAILED,	
+					message: `Validation error: ${err}`
+				})
+			} else {
+				const {email, password} = request.body
+				try {
+					const user = await User.findOne({
+						email						
+					})
+					if(user == null) {
+						jsonResponse({
+							response,
+							status: STATUS_FAILED,	
+							message: 'Cannot find user',
+							data: {}
+						})	
+					}
+					if(await checkPassword({password, hashedPassword: user.password}) == false) {
+						jsonResponse({
+							response,
+							status: STATUS_FAILED,	
+							message: 'Wrong email and password',
+							data: {email, id, name, tokenKey}
+						})
+					}					
+					jsonResponse({
+						response,
+						status: STATUS_SUCCESS,	
+						message: 'Register user successfully',
+						data: {email, id, name, tokenKey}
+					})
+				}catch(exception) {
+					jsonResponse({
+						response,
+						status: STATUS_FAILED,	
+						message: `Cannot register new user: ${err}`,
+						data: {}
+					})
+				}				
+			}
+		})		
 	})		
 }
